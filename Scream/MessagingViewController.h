c9b7978982b8766a96fb1045688d3e267826ee68
ColeHud <cmhudson11@gmail.com>
2015-05-23 19:27:07 -0400

    Custom messaging view controller

---
+++ Scream/MessagingViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagingViewController.h
+//  Scream
+//
+//  Created by Cole on 5/23/15.
+//  Copyright (c) 2015 Cole Hudson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MessagingViewController : UIViewController
+
+@end
